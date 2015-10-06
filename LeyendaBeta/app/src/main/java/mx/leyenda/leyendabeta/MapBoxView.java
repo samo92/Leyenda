5b7bb87756b763a43310c359eb3dea2d647deded
samo92 <samo-atem@hotmail.com>
2015-10-06 17:53:16 -0500

    feature geolocalizacion

--- LeyendaBeta/app/src/main/java/mx/leyenda/leyendabeta/MapBoxView.java
+++ LeyendaBeta/app/src/main/java/mx/leyenda/leyendabeta/MapBoxView.java
@@ -1,17 +1,33 @@
 package mx.leyenda.leyendabeta;
 
+import android.location.LocationProvider;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import com.mapbox.mapboxsdk.overlay.GpsLocationProvider;
+import com.mapbox.mapboxsdk.overlay.UserLocationOverlay;
+import com.mapbox.mapboxsdk.views.MapView;
 
 /**
  * Created by samo92 on 06/10/2015.
  */
 public class MapBoxView extends AppCompatActivity {
 
+    MapView myMapView;
+    GpsLocationProvider locationProvider;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //this.findViewById(R.id.mapview);
         setContentView(R.layout.mapbox_view);
+
+        myMapView = (MapView) findViewById(R.id.mapview);
+        locationProvider = new GpsLocationProvider(getApplicationContext());
+
+        UserLocationOverlay myLocationOverlay = new UserLocationOverlay(locationProvider, myMapView);
+        myLocationOverlay.enableMyLocation();
+        myLocationOverlay.setDrawAccuracyEnabled(true);
+        myMapView.getOverlays().add(myLocationOverlay);
     }
 }
