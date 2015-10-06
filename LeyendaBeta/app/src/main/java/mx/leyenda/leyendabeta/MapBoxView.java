fc5d93769dac07760b3361ced3d9b10621166bc4
samo92 <samo-atem@hotmail.com>
2015-10-06 17:30:15 -0500

    feature cargar mapa

---
+++ LeyendaBeta/app/src/main/java/mx/leyenda/leyendabeta/MapBoxView.java
@@ -0,0 +1,17 @@
+package mx.leyenda.leyendabeta;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by samo92 on 06/10/2015.
+ */
+public class MapBoxView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //this.findViewById(R.id.mapview);
+        setContentView(R.layout.mapbox_view);
+    }
+}
