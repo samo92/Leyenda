ae953c183e2807dc73df8ccffb741882f5df4fd0
LeonardoPC23 <pereyraleonardo23@gmail.com>
2015-10-07 14:10:52 -0500

    Validacion de estatus del GPS

--- LeyendaBeta/app/src/main/java/mx/leyenda/leyendabeta/MapBoxView.java
+++ LeyendaBeta/app/src/main/java/mx/leyenda/leyendabeta/MapBoxView.java
@@ -1,7 +1,13 @@
 package mx.leyenda.leyendabeta;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.LocationManager;
 import android.location.LocationProvider;
 import android.os.Bundle;
+import android.preference.DialogPreference;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 
 import com.mapbox.mapboxsdk.overlay.GpsLocationProvider;
@@ -15,13 +21,20 @@ public class MapBoxView extends AppCompatActivity {
 
     MapView myMapView;
     GpsLocationProvider locationProvider;
+    AlertDialog alertDialog = null;
+    LocationManager locationManager = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //this.findViewById(R.id.mapview);
         setContentView(R.layout.mapbox_view);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
+        //Validar estado de gps
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            AlertGPSoff();
+        }
         myMapView = (MapView) findViewById(R.id.mapview);
         locationProvider = new GpsLocationProvider(getApplicationContext());
 
@@ -37,5 +50,32 @@ public class MapBoxView extends AppCompatActivity {
         myLocationOverlay.setDrawAccuracyEnabled(true);
         myMapView.getOverlays().add(myLocationOverlay);
 
+    }
+
+    //Mensaje para activar GPS
+    private void AlertGPSoff() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("El sustema GPS esta desactivado, ¿Desea activarlo?")
+                .setCancelable(false)
+                .setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                    public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        dialog.cancel();
+                    }
+                });
+        alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (alertDialog != null) {
+            alertDialog.dismiss();
+        }
     }
 }
