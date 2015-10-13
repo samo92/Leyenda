a79e13432ff0339645aed9d1093aa447a7316cc3
samo92 <samo-atem@hotmail.com>
2015-10-12 20:32:13 -0500

    creacion de models y constant

---
+++ LeyendaBeta/app/src/main/java/mx/leyenda/leyendabeta/constant/Constant.java
@@ -0,0 +1,7 @@
+package mx.leyenda.leyendabeta.constant;
+
+/**
+ * Created by samo92 on 12/10/2015.
+ */
+public class Constant {
+}
---
+++ LeyendaBeta/app/src/main/java/mx/leyenda/leyendabeta/model/ApiService.java
@@ -0,0 +1,7 @@
+package mx.leyenda.leyendabeta.model;
+
+/**
+ * Created by samo92 on 12/10/2015.
+ */
+public interface ApiService {
+}
